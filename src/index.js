ea03b5d79ce72455f04fafa9938482f1fb80e14d
Han-Mi Kim <tmpham1@uci.edu>
2018-09-07 12:55:42 -0700

    set up layout for chat

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'; 
+import rootReducer from './reducers'; 
+
+const store = createStore(rootReducer);
 
 import App from './components/app';
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <Router>
+        <App/>
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
